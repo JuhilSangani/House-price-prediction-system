922008a4f4c1a5c2873a74a700dd5a0898f5cf6c
Juhil Sangani <58851904+JuhilSangani@users.noreply.github.com>
2021-05-04 20:51:38 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,45 +1,5 @@
 from flask import Flask, request, jsonify, render_template
-import json
-import pickle
-import numpy as np
-
-__locations = None
-__data_columns = None
-__model = None
-
-def get_predicted_price(location, sqft, bath, bhk):
-    try:
-        index_location = __data_columns.index(location.lower())
-    except:
-        index_location = -1
-
-    x = np.zeros(len(__data_columns))
-    x[0] = sqft
-    x[1] = bath
-    x[2] = bhk
-    if index_location >= 0:
-        x[index_location] = 1
-
-    return round(__model.predict([x])[0], 2)
-
-def get_location_util():
-    return __locations
-
-def load_saved_artifacts():
-    print("Loading saved artifacts...")
-    global __data_columns
-    global __locations
-    global __model
-
-    with open("./artifacts/columns.json", 'r') as f:
-        __data_columns = json.load(f)['data_columns']
-        __locations = __data_columns[3:]
-
-    with open("./artifacts/house_price_prediction_system.pickle", 'rb') as f:
-        __model = pickle.load(f)
-
-    print("..done")
-
+import util
 
 app = Flask(__name__, template_folder='')
 
@@ -47,10 +7,10 @@ app = Flask(__name__, template_folder='')
 def home():
     return render_template('app.html')
 
-@app.route("/")
+@app.route("/get_location")
 def get_location():
     response = jsonify({
-        'locations' : get_location_util()
+        'locations' : util.get_location()
     })
     response.headers.add('Access-Control-Allow-Origin','*')
 
@@ -64,7 +24,7 @@ def predict_house_price():
     bath = int(request.form['bath'])
 
     response = jsonify({
-        'predicted_price' : get_predicted_price(location, total_sqft, bhk, bath)
+        'predicted_price' : util.get_predicted_price(location, total_sqft, bhk, bath)
     })
     response.headers.add('Access-Control-Allow-Origin','*')
 
@@ -73,6 +33,5 @@ def predict_house_price():
 
 if __name__ == "__main__":
     print("Starting Python Flask server for House price prediction system")
-    load_saved_artifacts()
-    get_location_util()
-    app.run(debug=True)
+    util.load_saved_artifacts()
+    app.run()
